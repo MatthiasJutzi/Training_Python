7838fcb6c5eb10606231699807d93e9fb7647e97
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-02-11 15:35:10 +0100

    Create Card Printer file

---
+++ DataType/stringCommonMethod.py
@@ -0,0 +1,18 @@
+# upper()
+myStr1 = 'Hello World'
+uppercase_myStr1 = myStr1.upper()
+# print(uppercase_myStr1)
+
+myList1 = ['Red','Green','Blue']
+for i in myList1:
+    print(i)
+
+mySet1 = {'Red','Green','Blue'}
+for i in mySet1:
+    print(i)
+
+MyDict1 = {'name':'John','age':30,'city':'New York'}
+for value in MyDict1.values():
+    print(value)
+
+
