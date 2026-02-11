5ac132184e4b83fb1ec7d6e31bfc7df1d13bc43e
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-02-11 15:56:43 +0100

    Add simple method py

--- DataType/stringCommonMethod.py
+++ DataType/stringCommonMethod.py
@@ -1,7 +1,35 @@
 # upper()
 myStr1 = 'Hello World'
 uppercase_myStr1 = myStr1.upper()
-# print(uppercase_myStr1)
+print(uppercase_myStr1)
+
+# lower()
+myStr2 = 'Hello World'
+lowercase_myStr2 = myStr2.lower()
+print(lowercase_myStr2)
+
+# strip()
+myStr3 = ' hello world '
+stripmyStr3 = myStr3.strip()
+print(stripmyStr3)
+
+# replace(old, new)
+myStr4 = 'hello world'
+replacemyStr4 = myStr4.replace('hello', 'hi')
+print(replacemyStr4)
+
+# split()
+myStr5 = 'hello world'
+splitmyStr5 = myStr5.split()
+print(splitmyStr5)
+
+# join()
+myList1 = ['hello', 'world']
+joinmyList1 = ' '.join(myList1)
+print(joinmyList1)
+
+# startwith()
+
 
 myList1 = ['Red','Green','Blue']
 for i in myList1:
