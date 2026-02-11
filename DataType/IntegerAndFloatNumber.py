b3edd0b855145cb75afdc1e37b216580c3a0b2f1
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-02-11 16:15:16 +0100

    Sum of ints

--- DataType/IntegerAndFloatNumber.py
+++ DataType/IntegerAndFloatNumber.py
@@ -1,5 +1,7 @@
-myInt1 = 56
-myInt2 = -4
-print(type(myInt1)) # <class 'int'>
-print(type(myInt2)) # <class 'int'>
+my_int_1 = 56
+my_int_2 = -4
+print(type(my_int_1)) # <class 'int'>
+print(type(my_int_2)) # <class 'int'>
 
+sum_ints = my_int_1 + my_int_2
+print('Sum of int 1 and int 2 is: ', sum_ints) # 52
