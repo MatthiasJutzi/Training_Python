05d184b6b1a823ef38fc0331a3e9d32409a676d3
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-02-18 16:29:44 +0100

    Updating Employee_card Position and Salary

--- DataType/EMployeeProfileGenerator.py
+++ DataType/EMployeeProfileGenerator.py
@@ -9,5 +9,8 @@ print(employee_info)
 experience_years = 5
 experience_info = 'Experience: ' + str(experience_years) + ' years'
 print(experience_info)
-employee_card = f'Employee: {full_name}'
+position = "Data Analyst"
+salary = 75000
+employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
 print(employee_card)
+
