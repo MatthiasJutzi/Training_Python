cb9a89e987fa1bae240c17c21733f36ff5c8c342
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-02-18 16:33:13 +0100

    Year_code and initials slicing

--- DataType/EMployeeProfileGenerator.py
+++ DataType/EMployeeProfileGenerator.py
@@ -6,9 +6,11 @@ address += ', Apartment 4B'
 employee_age = 28
 employee_info = full_name + ' is ' + str(employee_age) + ' years old'
 print(employee_info)
+
 experience_years = 5
 experience_info = 'Experience: ' + str(experience_years) + ' years'
 print(experience_info)
+
 position = "Data Analyst"
 salary = 75000
 employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
@@ -17,3 +19,8 @@ print(employee_card)
 employee_code = 'DEV-2026-JD-001'
 department = employee_code[0:3]
 print(department)
+
+year_code = employee_code[4:8]
+initials= employee_code[9:11]
+print(year_code) 
+print(initials)
