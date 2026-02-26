483085a2792bbc75347cd9e14a917150be0ed559
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-02-26 10:42:11 +0100

    Refactor print statements for integer and float operations for clarity

--- DataType/IntegerAndFloatNumber.py
+++ DataType/IntegerAndFloatNumber.py
@@ -2,6 +2,64 @@ my_int_1 = 56
 my_int_2 = -4
 print(type(my_int_1)) # <class 'int'>
 print(type(my_int_2)) # <class 'int'>
-
+# Integer Addition
 sum_ints = my_int_1 + my_int_2
-print('Sum of int 1 and int 2 is: ', sum_ints) # 52
+print('Integer Addition: ', sum_ints) # 52
+
+my_int_1 = 56 
+my_int_2 = 12
+# Subtraction
+sub_ints = my_int_1 - my_int_2 
+print('Integer Subtraction: ', sub_ints) # 44
+
+my_int_1 = 12
+my_int_2 = 4
+
+# Multiplication
+product_ints = my_int_1 * my_int_2
+print('Integer Multiplication:', product_ints)  # 48
+
+my_int_1 = 56
+my_int_2 = 12
+
+# Division
+div_ints = my_int_1 / my_int_2
+print('Integer Division:', div_ints) # 4.666666666666667
+
+my_float_1 = -12.0
+my_float_2 = 4.9
+
+print(type(my_float_1)) # <class 'float'>
+print(type(my_float_2)) # <class 'float'>
+
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+float_addition = my_float_1 + my_float_2
+print('Float Addition:', float_addition) # 17.4
+
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+float_subtraction = my_float_2 - my_float_1
+print('Float Subtraction:', float_subtraction) # 6.6
+
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+float_multiplication = my_float_2 * my_float_1
+print('Float Multiplication:', float_multiplication) # 64.8
+
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+float_division = my_float_2 / my_float_1
+print('Float Division:', float_division) # 2.2222222222222223
+
+my_int = 56
+my_float = 5.4
+
+sum_int_and_float = my_int + my_float
+
+print(sum_int_and_float) # 61.4
+print(type(sum_int_and_float)) # <class 'float'>
