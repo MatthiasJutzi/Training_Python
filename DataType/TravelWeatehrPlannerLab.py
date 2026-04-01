50fda2f0c7f6830e1f4bab5c8505ea93150a8a6e
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-01 17:03:44 +0200

    Ajout de la logique de planification météorologique dans TravelWeatherPlannerLab.py

--- DataType/TravelWeatehrPlannerLab.py
+++ DataType/TravelWeatehrPlannerLab.py
@@ -1,3 +1,28 @@
 #Start of exercise
 #Travel Weather Planner
 
+# Variables:
+distance_mi = 0
+is_raining = False
+has_bike = False
+has_car = False
+has_ride_share_app = False
+
+if distance_mi == 0:
+    print(False)
+elif distance_mi <= 1 and is_raining == False:
+    print(True)
+else:
+    print(False)
+
+if distance_mi > 1 and distance_mi <= 6:
+    print(True)
+else:
+    print(False)
+
+if distance_mi > 6 and (has_car == True or has_bike== True):
+    print(True)
+else:    
+    print(False)
+
+
