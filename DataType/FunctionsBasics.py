e5fa668341adce6110b37daa65579dcdd5406cf9
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-13 17:15:34 +0200

    creating basic function file

---
+++ DataType/FunctionsBasics.py
@@ -0,0 +1,10 @@
+# input() function is used to take input from the user. It always returns a string.
+name = input("What is your name? ")
+print("Hello " + name)
+
+# int() converts a number, boolean, and a numeric string into an integer:
+print(int(3.14)) # 3
+print(int('42')) # 42
+print(int(True)) # 1
+print(int(False)) # 0 
+
