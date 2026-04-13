9fdc94f04366a8623163a24002a315fe443cf9b0
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-13 17:21:11 +0200

    custom function call

--- DataType/FunctionsBasics.py
+++ DataType/FunctionsBasics.py
@@ -8,3 +8,28 @@ print(int('42')) # 42
 print(int(True)) # 1
 print(int(False)) # 0 
 
+# for custom function we uuse def keyword:
+def hello():
+    print("Hello World")
+
+hello() # calling the function to execute the code inside it
+
+# Exampe of function for an addition of two numbers:
+def calculate_sum(a,b):
+    print(a+b)
+
+calculate_sum(1,2) #3
+
+# Not the good way to use function, because it does not return any value, it just prints the result. We can use return statement to return the result from the function:
+def calculate_sum(a, b):
+    print(a + b)
+
+my_sum = calculate_sum(3, 1) # 4
+print(my_sum) # None
+
+# Good way to use function with return statement:
+def calculate_sum(a, b):
+    return a + b    
+my_sum = calculate_sum(3, 1) # 4
+print(my_sum) # 4
+
