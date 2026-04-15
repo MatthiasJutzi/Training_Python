387d52e6265966fe44f97e5c4f6924d5cf11d98b
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-15 17:29:22 +0200

    Final, tests passed

--- DataType/ApplyDiscountFunction.py
+++ DataType/ApplyDiscountFunction.py
@@ -1,8 +1,40 @@
 # Build an Apply Discount Function
-
-def applay_discount(price,discount):
-    if price != int or price != float:
-        return print("The price should be a number")
-    elif discount != int or discount != float:
-        return print("The discount should be a number")
+# Try 1
+# def apply_discount(price,discount):
+#     if price != int or price != float:
+#         return "The price should be a number"
+#     elif discount != int or discount != float:
+#         return "The discount should be a number"
+#     elif price <= 0 
+#         return "The price should be greater than 0"
+#     elif discount < 0 or discount > 100:
+#        return "The discount should be between 0 and 100"
+#     else : 
+#         discount_price = price - (price * discount / 100)
+#         return discount_price
     
+# price = int(input("Enter the price: "))
+# discount = int(input("Enter the discount percentage: ")) 
+# apply_discount(price,discount)
+
+
+# try 2
+# isinstance() in python is a built-in function that checks if an object is an instance of a specified class or a tuple of classes. 
+# It returns True if the object is an instance of the class or any subclass thereof, and False otherwise.
+
+def apply_discount(price, discount):
+    if not isinstance(price, (int, float)):
+        return "The price should be a number"
+    elif not isinstance(discount, (int, float)):
+        return "The discount should be a number"
+    elif price <= 0:
+        return "The price should be greater than 0"
+    elif discount < 0 or discount > 100:
+        return "The discount should be between 0 and 100"
+    else:
+        discount_price = price - (price * (discount / 100))
+        return discount_price
+
+price = float(input("Enter the price: "))
+discount = float(input("Enter the discount percentage: "))
+print(apply_discount(price, discount))
