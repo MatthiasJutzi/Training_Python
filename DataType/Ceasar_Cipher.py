69a741ec3bdf60e3ef64175a1cf69164c4627f96
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-16 13:19:09 +0200

    Ceasar step 4

--- DataType/Ceasar_Cipher.py
+++ DataType/Ceasar_Cipher.py
@@ -1,5 +1,5 @@
 # Ceasar Cipher lab
 shift = 5
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-shifted_alphabet = alphabet[shift:]
+shifted_alphabet = alphabet[shift:] + alphabet[:shift]
 print(shifted_alphabet)
