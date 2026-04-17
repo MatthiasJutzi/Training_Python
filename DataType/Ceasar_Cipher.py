fc0e0fae76b261da0cb3d6f1b201c7b87e935b81
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-17 11:22:27 +0200

    Caeser Function update

--- DataType/Ceasar_Cipher.py
+++ DataType/Ceasar_Cipher.py
@@ -7,13 +7,15 @@
 # encrypted_text = text.translate(translation_table)
 # print(encrypted_text)
 
-def caesar():
-    shift = 5
+def caesar(text, shift):
+
+    if isinstance(shift, int):
+        return "Shift must be an integer value."
+    
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    translation_table = str.maketrans(alphabet, shifted_alphabet)
-    text = "hello world"
-    encrypted_text = text.translate(translation_table)
-    print(encrypted_text)
-
-caesar()
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    return text.translate(translation_table)
+   
+encrypted_text = caesar("freeCodeCamp", 3)
+print(encrypted_text)
