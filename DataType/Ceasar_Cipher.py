18c9bead89daec3ddb21050fbd4581b9292067a5
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-17 11:00:03 +0200

    Caessar update

--- DataType/Ceasar_Cipher.py
+++ DataType/Ceasar_Cipher.py
@@ -2,3 +2,16 @@
 shift = 5
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+translation_table = str.maketrans(alphabet, shifted_alphabet)
+text = "hello world"
+encrypted_text = text.translate(translation_table)
+print(encrypted_text)
+
+def caesar():
+    shift = 5
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet, shifted_alphabet)
+    text = "hello world"
+    encrypted_text = text.translate(translation_table)
+    print(encrypted_text)
