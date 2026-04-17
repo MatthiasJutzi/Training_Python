543dbdb7b5374aae53a1cd1d9dd7207f6a5a3a8f
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-17 11:07:21 +0200

    function caesar

--- DataType/Ceasar_Cipher.py
+++ DataType/Ceasar_Cipher.py
@@ -1,11 +1,11 @@
-# Ceasar Cipher lab
-shift = 5
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-translation_table = str.maketrans(alphabet, shifted_alphabet)
-text = "hello world"
-encrypted_text = text.translate(translation_table)
-print(encrypted_text)
+# Ceasar Cipher lab ctrl+k ctrl+c for comment ctrl+k ctrl+u for uncomment
+# shift = 5
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+# translation_table = str.maketrans(alphabet, shifted_alphabet)
+# text = "hello world"
+# encrypted_text = text.translate(translation_table)
+# print(encrypted_text)
 
 def caesar():
     shift = 5
@@ -15,3 +15,5 @@ def caesar():
     text = "hello world"
     encrypted_text = text.translate(translation_table)
     print(encrypted_text)
+
+caesar()
