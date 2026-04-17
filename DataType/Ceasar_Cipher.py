aac3d107d2689e3b5fc1ec218c90be28b8f0b421
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-17 11:36:04 +0200

    Final Caesar

--- DataType/Ceasar_Cipher.py
+++ DataType/Ceasar_Cipher.py
@@ -29,5 +29,7 @@ def encrypt(text, shift):
 def decrypt(text, shift):
     return caesar(text, shift, encrypt=False)
 
-encrypted_text = caesar("freeCodeCamp", 3)
+encrypted_text = "Pbhentr vf sbhaq va hayvxryl cynprf."
+decrypted_text = decrypt(encrypted_text, 13)
 print(encrypted_text)
+print(decrypted_text)
