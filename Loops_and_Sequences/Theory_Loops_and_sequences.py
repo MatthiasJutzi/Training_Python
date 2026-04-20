c11405a57e7839cf782dedce4f7fa655422e6249
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-20 17:33:43 +0200

    List basic

--- Loops_and_Sequences/Theory_Loops_and_sequences.py
+++ Loops_and_Sequences/Theory_Loops_and_sequences.py
@@ -1,3 +1,14 @@
 # Theory loops and sequences
 # Loops are used to repeat a block of code multiple times. There are two types of loops in Python: for loops and while loops.
 
+# exemple of a list
+swiss_cities = ["Zurich", "Geneva", "Basel", "Bern"]
+
+swiss_cities[0]
+print(swiss_cities[0]) # ? => 'Zurich', because list indexing starts at 0, so swiss_cities[0] refers to the first element of the list, which is 'Zurich'.
+swiss_cities[-1]
+print(swiss_cities[-1]) # ? => 'Bern', because negative indexing starts from the end of the list, so swiss_cities[-1] refers to the last element of the list, which is 'Bern'.
+
+citie_name = "Geneva"
+list(citie_name) # ? => ['G', 'e', 'n', 'e', 'v', 'a'], because the list() function converts a string into a list of its individual characters.
+
