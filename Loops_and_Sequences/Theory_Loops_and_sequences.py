dcd0df919887dd502319363f48e8ceeb5d74819c
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-20 17:36:11 +0200

    basic loops update

--- Loops_and_Sequences/Theory_Loops_and_sequences.py
+++ Loops_and_Sequences/Theory_Loops_and_sequences.py
@@ -12,3 +12,11 @@ print(swiss_cities[-1]) # ? => 'Bern', because negative indexing starts from the
 citie_name = "Geneva"
 list(citie_name) # ? => ['G', 'e', 'n', 'e', 'v', 'a'], because the list() function converts a string into a list of its individual characters.
 
+# to calculate the length of a list, we can use the len() function
+numbers_list = [1, 2, 3, 4, 5]
+len(numbers_list) # ? => 5, because the len() function returns the number of items in the list, and there are 5 items in numbers_list.
+
+#to update the avalue of an element in a list, we can use indexing to assign a new value to that index. For example:
+swiss_cities[1] = "Lausanne"
+print(swiss_cities) # ? => ['Zurich', 'Lausanne', 'Basel', 'Bern'], because we assigned the value "Lausanne" to the index 1 of the swiss_cities list, which replaced the previous value "Geneva".
+
