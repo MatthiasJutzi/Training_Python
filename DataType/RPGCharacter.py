b313e8530caaa348721afc1552f0cecd81c17db4
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-20 15:44:08 +0200

    Name validation

--- DataType/RPGCharacter.py
+++ DataType/RPGCharacter.py
@@ -1,5 +1,15 @@
 # Character builder function in python 
 
 def create_character(name, strength, intelligence, charisma):
+    # Name validation
     if not isinstance(name, str):
         return "The character name should be a string"
+    if name == "":
+        return "The character should have a name"
+    if len(name) > 10:
+        return "The character name is too long"
+    if ' ' in name:
+        return "The character name should not contain spaces"
+    
+    # Stats validation
+    
