7145d311eaddc302f00c1893a7fd20f899a5302a
Matthias Jutzi <matthias.jutzi@bluewin.ch>
2026-04-28 11:41:35 +0200

    for while break in loop

--- Loops_and_Sequences/Theory_Loops_and_sequences.py
+++ Loops_and_Sequences/Theory_Loops_and_sequences.py
@@ -307,3 +307,41 @@ Wrong! Try again.
 Guess the number (1-5): 3
 You got it!
 """
+
+# Just like in JavaScript, Python supports the break and continue statements.
+# The break statement is used to stop the execution of a loop. 
+# Here is an example of using the break statement for a list of developer_names:
+developer_names = ['Jess', 'Naomi', 'Tom']
+
+for developer in developer_names:
+    if developer == 'Naomi':
+        break
+    print(developer)
+
+
+# The continue statement is used to skip the current iteration of a loop and move onto the next iteration. 
+#Let's modify the example from earlier to use the continue statement instead of break
+developer_names = ['Jess', 'Naomi', 'Tom']
+
+for developer in developer_names:
+    if developer == 'Naomi':
+        continue
+    print(developer)
+
+words = ['sky', 'apple', 'rhythm', 'fly', 'orange']
+
+for word in words:
+    for letter in word:
+        if letter.lower() in 'aeiou':
+            print(f"'{word}' contains the vowel '{letter}'")
+            break
+    else:
+        print(f"'{word}' has no vowels")
+
+"""
+'sky' has no vowels
+'apple' contains the vowel 'a'
+'rhythm' has no vowels
+'fly' has no vowels
+'orange' contains the vowel 'o'
+"""
